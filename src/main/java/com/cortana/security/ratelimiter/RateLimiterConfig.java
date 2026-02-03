285b09007ff7eab518737c52ac37a9d6593ea912
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2026-02-03 15:34:01 +0100

    feat: add initial configuration for rate limiting

---
+++ src/main/java/com/cortana/security/ratelimiter/RateLimiterConfig.java
@@ -0,0 +1,13 @@
+package com.cortana.security.ratelimiter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RateLimiterConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // registry.addInterceptor(new RateLimitingInterceptor());
+    }
+}
