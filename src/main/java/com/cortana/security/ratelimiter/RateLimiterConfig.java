ecd91551908e4b80c50ccc01e4ae507c6d0fbb52
Olivier Robert <olivier.robert@brainfeed.com>
2026-02-06 13:37:15 +0100

    Refactor: Update authorship and project metadata

--- src/main/java/com/cortana/security/ratelimiter/RateLimiterConfig.java
+++ src/main/java/com/cortana/security/ratelimiter/RateLimiterConfig.java
@@ -1,4 +1,4 @@
-package com.cortana.security.ratelimiter;
+package com..security.ratelimiter;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
